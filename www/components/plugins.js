fd52fe9deef21ec5ef805d803436a2fe60ae5e9e
GuilhermeJoseSantosdaSilva <gujose00@gmail.com>
2020-05-29 02:02:45 +0900

    ComeçandoCamera

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -52,3 +52,20 @@ $(document).on("click", "#local", function(){
   navigator.geolocation.getCurrentPosition(onSuccess, onError);
 });
 
+$(document).on("click", "#camera", function(){
+  navigator.camera.getPicture(onSuccess, onFail, { 
+    quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    correctOrientation: true,
+    savetoPhotoAlbum: true 
+    });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('imagem');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+});
