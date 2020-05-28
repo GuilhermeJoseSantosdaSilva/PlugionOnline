f16e3ddac5404ae6beafe532bbe75f577fe99ae9
GuilhermeJoseSantosdaSilva <gujose00@gmail.com>
2020-05-29 02:32:13 +0900

    Barcode

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -69,3 +69,30 @@ function onFail(message) {
     alert('Failed because: ' + message);
 }
 });
+
+$(document).on("click", "#cdg_barra", function(){
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Escaneia alguma coisa ai amigão", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+});
